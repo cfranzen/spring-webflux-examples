0b71c198597e2c68de4aa8227c695e6c10cbc9d4
Christian Franzen <13965040+cfranzen@users.noreply.github.com>
2023-12-08 11:32:17 +0100

    Replace R2dbcRepository with ReactiveCrudRepository

--- spring-webflux-plain-java-example/src/main/java/de/cfranzen/examples/webflux/CustomerRepository.java
+++ spring-webflux-plain-java-example/src/main/java/de/cfranzen/examples/webflux/CustomerRepository.java
@@ -1,11 +1,11 @@
 package de.cfranzen.examples.webflux;
 
-import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 
 @Repository
-public interface CustomerRepository extends R2dbcRepository<Customer, Long> {
+public interface CustomerRepository extends ReactiveCrudRepository<Customer, Long> {
 
     Flux<Customer> findByFirstNameAndLastName(String firstName, String lastName);
 }
